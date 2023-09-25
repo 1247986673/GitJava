02c1498a5ecf59199f643f49e97dc1dafb7401f2
bcq <bcq@124>
2023-09-25 21:31:22 +0800

    second commit

--- src/main/java/com/bcq/ph/gitTest.java
+++ src/main/java/com/bcq/ph/gitTest.java
@@ -3,5 +3,6 @@ package com.bcq.ph;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
